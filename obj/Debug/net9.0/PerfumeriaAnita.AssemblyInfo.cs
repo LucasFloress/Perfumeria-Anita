4847c55eaa1968c363b0230e7d1942060bbe091b
Lucas Ezequiel Flores <floreslucas125@gmail.com>
2026-06-01 18:55:04 +0000

    Feature: Archivos raros

--- obj/Debug/net9.0/PerfumeriaAnita.AssemblyInfo.cs
+++ obj/Debug/net9.0/PerfumeriaAnita.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerfumeriaAnita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4645ed560ff8893d2361b72878e85f86070f378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60ec68c090f861fb12d8661b35901c0ec72873d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerfumeriaAnita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerfumeriaAnita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
